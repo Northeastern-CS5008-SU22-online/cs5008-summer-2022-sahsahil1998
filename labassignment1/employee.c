c3bf8fee107e60f1a40733c4d0c76ab01a8aa25a
Sahil Sah <sahilsah@login-students.ccs.neu.edu>
2022-05-22 21:11:49 -0400

    upload lab assignment 1

--- labassignment1/employee.c
+++ labassignment1/employee.c
@@ -1,3 +1,6 @@
+//Sahil Sah
+//sah.sa@northeastern.edu
+
 /*C Program to read and print the employee details using structure and dynamic memory allocation*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,11 +16,29 @@ typedef struct employee
 /* Read the employee details using scanf*/
 void readData(int n, employee_t* s)
 {
+    int i;
+    for( i = 0; i < n; i++){
+        printf("Enter the employeeId, Name,designation and Department of Employee %d:\n", i+1);
+	scanf("%d",&s[i].empId);
+	scanf("%s",&s[i].Name);
+	scanf("%s",&s[i].Designation);
+	scanf("%s",&s[i].Dept);	     
+
+    }
     
 }
 /* Function to print the employee details*/
 void display(int n, employee_t * s)
 {
+    int i;
+    for( i = 0; i < n; i++){
+         printf("The details of Employee %d\n", i+1);
+         printf("empID: %d\n",s[i].empId);
+         printf("Name: %s\n",s[i].Name);
+         printf("Designation: %s\n",s[i].Designation);
+         printf("Department: %s\n",s[i].Dept);
+
+    }
     
     
 }
@@ -27,7 +48,15 @@ int main()
 
 {
 
-    
-
+	int n; //number of employees
+	printf("Enter the number of employees\n");
+	scanf("%d",&n);    
+	employee_t *s = (employee_t*)malloc(n* sizeof(employee_t));
+	if(s != NULL){
+	     readData(n,s);
+	     display(n,s);
+	     free(s);			
+	}
+	return 0;
 }
 
