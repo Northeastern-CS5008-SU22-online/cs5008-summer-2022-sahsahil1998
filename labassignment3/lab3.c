27dfb53f449d62a6a5025e678e383211bb53f039
Sahil Sah <sahilsah@login-students.ccs.neu.edu>
2022-06-07 18:02:51 -0400

    upload lab assignment 3

--- labassignment3/lab3.c
+++ labassignment3/lab3.c
@@ -1,20 +1,45 @@
-//enter your name here
-//enter your email here
+//Sahil Sah
+//sah.sa@northeastern.edu
 
 #include <stdio.h>
-#define MAX 8
+#include <stdlib.h>
+#define MAX 11
 int main()
 {
-    int arr[MAX] = {80,35,56,45,10,90,12,67};
-    int i,j,temp;
-    
-    //insert your code here
-    
-    
-    
-    
-    
-    
-    
-    return 0;
+
+	FILE *fp;
+
+	int arr[MAX];
+	int i,j,temp;
+	//take in and read input file
+	fp = fopen("Input.txt","r");
+	if(fp==NULL){
+		printf("\nUnable to open the file\n");
+		exit(0);
+	}
+	for(i=0;i < MAX; i++){
+		fscanf(fp, "%d ", &arr[i]);
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	//sort
+	for (i=0; i<MAX; i++){
+		j = i - 1;
+		temp = arr[i];
+		while(temp < arr[j] && j >=0){
+			arr[j+1] = arr[j];
+			j--;
+		}
+		arr[j+1] = temp;
+	}
+	//output file
+	fp = fopen("sorted.txt", "w");
+	printf("The sorted output is:\n");
+	for(i=0;i<MAX;i++){
+		fprintf(fp, "%d\n", arr[i]);
+		printf("%d ", arr[i]);	
+	}
+	printf("\n");
+	fclose(fp);
+	return 0;
 }
