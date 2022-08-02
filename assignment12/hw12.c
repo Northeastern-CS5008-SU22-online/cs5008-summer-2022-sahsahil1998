11a7b4bcaa54d6ee1d17e08bf3922dbe2e9d5d4c
Sahil Sah <sahilsah@login-students.ccs.neu.edu>
2022-08-02 13:38:41 -0400

    upload assignment 12

--- assignment12/hw12.c
+++ assignment12/hw12.c
@@ -1,5 +1,5 @@
-// name: <your name here>
-// email: <your email here>
+// name: Sahil Sah
+// email: sah.sa@northeastern.edu
 // Compile with:
 //
 // gcc -lpthread hw12.c -o hw12
@@ -26,16 +26,22 @@ void* thread1 (void* vargp) {
 void* thread2 (void* vargp) {
   // add 5 to counter
   // *** YOUR CODE GOES HERE ***
+  counter = counter + 5;
+  return NULL;
 }
 
 void* thread3 (void* vargp) {
   // subtract 2 from counter
   // *** YOUR CODE GOES HERE ***
+  counter = counter - 2;
+  return NULL;
 }
 
 void* thread4 (void* vargp) {
   // subtract 10 from counter
   // *** YOUR CODE GOES HERE ***
+  counter = counter - 10;
+  return NULL;
 }
 
 int main() {
@@ -53,6 +59,9 @@ int main() {
     // now create the 2nd, 3rd, 4th group of 100 threads
     
     // *** YOUR CODE GOES HERE ***
+    pthread_create(&(tid[i + NTHREADS]), NULL, thread2, NULL);
+    pthread_create(&(tid[i + NTHREADS * 2]), NULL, thread3, NULL);
+    pthread_create(&(tid[i + NTHREADS * 3]), NULL, thread4, NULL);
   }
 
   //wait until ALL 400 threads are done
