0d89976ca5d3c9ec14c929725ae3ec6a213c26c1
Sahil Sah <sahilsah@login-students.ccs.neu.edu>
2022-08-02 18:00:19 -0400

    upload lab assignment 11

--- labassignment11/lab11.c
+++ labassignment11/lab11.c
@@ -1,5 +1,5 @@
-//Enter your name here
-//Enter your email here
+//Sahil Sah
+//sah.sa@northeastern.edu
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
@@ -13,26 +13,42 @@ void *philosopher(void *x)
     int n=*a;
     
         /*-----Insert your code here----*/
-    
-    
+    pthread_mutex_lock(&chopstick[n]);
+    pthread_mutex_lock(&chopstick[(n+1)%5]);
+
+    printf("Philosopher %d is eating using chopstick[%d] and chopstick[%d]\n",n, n, n+1);
+
+	sleep(1);
+	 pthread_mutex_unlock(&chopstick[n]);
+	 pthread_mutex_unlock(&chopstick[(n+1)%5]);
+	 printf("Philosopher %d is thinking\n",n);
+	sleep(1);   
+ 	return NULL;   
 }
 
 
 /*------------ Main Program---------*/
 int main()
 {
+    	pthread_t threads[5];
+    	int i,val[5];
+	for(i=0;i<5;i++){
+pthread_mutex_init(&chopstick[i],NULL);
+}   
+for(i=0;i<5;i++){
+    val[i]=i;
+    pthread_create(&threads[i],NULL,(void*)philosopher,&val[i]);
+} 
     
-//initializing the mutex
-    
-    
-//create and run the thread
-    
-    
-//wait until all the threads are done
-    
-    
-// Destroying the mutex
-    
+//wait until threads done
+	for(i=0;i<5;i++){
+    pthread_join(threads[i],NULL);
+}
+//destory mutex
+
+	for(i=0;i<5;i++){
+    		pthread_mutex_destroy(&chopstick[i]);
+}  
     
     return 0;
 }
